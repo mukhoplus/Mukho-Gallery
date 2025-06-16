61185e98dc8d2f1db91c4d091cb6e50045a19947
Mukho <mukho@kakao.com>
2025-06-16 15:56:07 +0900

    Style: Fix some css in LoginLinkRow

--- components/login/LoginLinkRow.tsx
+++ components/login/LoginLinkRow.tsx
@@ -12,27 +12,27 @@ export default function LoginLinkRow({
   onRegister,
 }: LoginLinkRowProps) {
   return (
-    <div className="mt-6 text-center space-x-4 flex justify-center">
+    <div className="mt-6 flex items-center justify-center space-x-4 text-center">
       <span
-        className="text-sm text-gray-600 cursor-pointer hover:text-emerald-600 transition-colors"
+        className="text-sm text-gray-600 cursor-pointer hover:text-emerald-600 transition-colors flex items-center"
         tabIndex={0}
         role="button"
         onClick={onFindEmail}
       >
         아이디 찾기
       </span>
-      <span className="text-gray-400">|</span>
+      <span className="text-gray-400 flex items-center">|</span>
       <span
-        className="text-sm text-gray-600 cursor-pointer hover:text-emerald-600 transition-colors"
+        className="text-sm text-gray-600 cursor-pointer hover:text-emerald-600 transition-colors flex items-center"
         tabIndex={0}
         role="button"
         onClick={onFindPw}
       >
         비밀번호 찾기
       </span>
-      <span className="text-gray-400">|</span>
+      <span className="text-gray-400 flex items-center">|</span>
       <span
-        className="text-sm text-gray-600 cursor-pointer hover:text-emerald-600 transition-colors"
+        className="text-sm text-gray-600 cursor-pointer hover:text-emerald-600 transition-colors flex items-center"
         tabIndex={0}
         role="button"
         onClick={onRegister}
